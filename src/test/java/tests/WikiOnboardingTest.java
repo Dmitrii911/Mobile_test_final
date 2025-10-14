3724cea3ad392431dadaa4565f39b26c53eddd27
Dmitrii Oleinik <wedil@mail.ru>
2025-10-15 00:08:45 +0500

    add

---
+++ src/test/java/tests/WikiOnboardingTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import screens.FirstOnboardingScreen;
+import screens.FourthOnboardingScreen;
+import screens.SecondOnboardingScreen;
+import screens.ThirdOnboardingScreen;
+
+import static io.qameta.allure.Allure.step;
+
+public class WikiOnboardingTest extends TestBase {
+
+    FirstOnboardingScreen firstScreen = new FirstOnboardingScreen();
+    SecondOnboardingScreen secondScreen = new SecondOnboardingScreen();
+    ThirdOnboardingScreen thirdScreen = new ThirdOnboardingScreen();
+    FourthOnboardingScreen fourthScreen = new FourthOnboardingScreen();
+
+
+    @Test
+    @DisplayName("Проверка экранов онбординга")
+    void verifyGettingStartedScreensTest() {
+        step("Проверить первый экран", () -> {
+            firstScreen.verifyFirstScreen();
+            firstScreen.goToNextScreen();
+        });
+
+        step("Проверить второй экран", () -> {
+            secondScreen.verifySecondScreen();
+            secondScreen.goToNextScreen();
+        });
+
+        step("Проверить третий экран", () -> {
+            thirdScreen.verifyThirdScreen();
+            thirdScreen.goToNextScreen();
+        });
+
+        step("Проверить четвертый экран", () -> {
+            fourthScreen.verifyFourthScreen();
+        });
+    }
+}
